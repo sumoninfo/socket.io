f488847e5278ff501c1f8ad7ad4d2faf3db598c9
Ekramul Sumon <38817030+sumoninfo@users.noreply.github.com>
2022-02-13 14:32:17 +0600

    Namespace with broadcast socket

--- index.js
+++ index.js
@@ -7,28 +7,37 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', (socket) => {
+let buyNsp = io.of("/buy")
+buyNsp.on('connection', function (socket) {
+    buyNsp.emit("buyNspEvent", "message send from buy nsp event")
+})
+
+let sellNsp = io.of("/sell")
+sellNsp.on('connection', function (socket) {
+    sellNsp.emit("sellNspEvent", "Message send from sell nsp event")
+})
+/*io.on('connection', (socket) => {
     io.sockets.emit("MyBroadcast", "Hello Everyone welcome")
 
-    /* socket.on('customEvent', function (msg) {
+    /!* socket.on('customEvent', function (msg) {
          console.log(msg);
-     })*/
-    /*socket.on('chat_message', msg => {
+     })*!/
+    /!*socket.on('chat_message', msg => {
         io.emit('chat_message', msg);
-    });*/
+    });*!/
     console.log('User connected');
     // setTimeout(function () {
     //     socket.send("hello sumon")
     // }, 2000)
 
 
-    /*setInterval(function () {
+    /!*setInterval(function () {
         let d = new Date();
         let t = d.getTime();
         console.log(t, 'time')
         socket.emit('myEvent', t)
-    }, 500)*/
-});
+    }, 500)*!/
+});*/
 
 http.listen(port, () => {
     console.log(`Socket.IO server running at http://localhost:${port}/`);
