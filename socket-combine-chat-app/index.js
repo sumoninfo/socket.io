e49759bf9f859957f74dce727edbec40161313a0
Ekramul Sumon <38817030+sumoninfo@users.noreply.github.com>
2022-02-13 19:54:19 +0600

    socket-combine-chat-app design updated

--- socket-combine-chat-app/index.js
+++ socket-combine-chat-app/index.js
@@ -3,11 +3,21 @@ const app           = express()
 const http          = require('http')
 const expressServer = http.createServer(app);
 
+const {Server} = require('socket.io')
+const io       = new Server(expressServer);
 
-app.get('/', function (req, res) {
+app.get('/', (req, res) => {
     res.sendFile(__dirname + "/index.html")
 })
 
-expressServer.listen(3000, function () {
+
+io.on('connection', (socket) => {
+    socket.on('chat', (msg) => {
+        io.emit('chat_transfer', msg)
+    })
+    console.log("New User Connected")
+})
+
+expressServer.listen(3000, () => {
     console.log('express server run @ 3000')
 })
